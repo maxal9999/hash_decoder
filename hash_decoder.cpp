f9abbb9dbeb2cb00efba0520f109550f37aa96c7
maxal9999 <maxal9999@mail.ru>
2019-08-13 10:35:47 +0300

    Add async mode

--- hash_decoder.cpp
+++ hash_decoder.cpp
@@ -11,10 +11,10 @@ int main(int argc, char* argv[])
 {
     MD5 md5;
     //auto encode = md5.digestString("HELLO THERE I AM MD5!");
-    auto encode = md5.digestString("HEL");
+    char* encode = "b67af3651497e0d3ef0fde55308a7d0e";//md5.digestString("HELO");
     std::cout << "original hash: " << encode << "\n";
 
-    int max = 3;
+    int max = 4;
 
     utils::MD5Decoder decoder(encode, max);
     auto res = decoder.Execute();
--- sources/decoder.cpp
+++ sources/decoder.cpp
@@ -7,8 +7,11 @@
 
 #include <algorithm>
 #include <array>
+#include <chrono>
+#include <future>
 #include <iostream>
 #include <thread>
+#include <vector>
 
 
 namespace utils
@@ -16,6 +19,8 @@ namespace utils
 
 namespace
 {
+
+// 65-90:97-122
 
 constexpr std::array<char, 60> ALL_CHARS = {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -35,6 +40,63 @@ bool IsUpper(const std::string& tmp_str)
 void ToUpper(std::string& tmp_str)
 {
     std::transform(tmp_str.begin(), tmp_str.end(), tmp_str.begin(), ::toupper);
+}
+
+}
+
+namespace
+{
+
+using FutRes = std::future<Result>;
+
+Result CaclAsyncHash(std::vector<FutRes>& futures)
+{
+    Result result;
+
+    static auto is_future_valid = [](FutRes& fut)
+    {
+        return fut.valid();
+    };
+
+    static auto is_future_ready = [&](FutRes& fut)
+    {
+        return is_future_valid(fut) &&
+               fut.wait_for(std::chrono::microseconds(100)) == std::future_status::ready;
+    };
+
+    static auto is_success = [&]() mutable
+    {
+        auto ready_it = std::find_if(futures.begin(), futures.end(), is_future_ready);
+        if (ready_it == futures.end())
+            return false;
+
+        auto tmp_res = ready_it->get();
+        if(tmp_res.mResultType == ResultType::SUCCESS)
+        {
+            result = std::move(tmp_res);
+            return true;
+        }
+
+        return false;
+    };
+
+    std::for_each(futures.begin(), futures.end(), [&](FutRes& fut){
+        auto tmp_res = fut.get();
+        if(tmp_res.mResultType == ResultType::SUCCESS)
+            result = std::move(tmp_res);
+    });
+
+    /*while (std::any_of(futures.begin(), futures.end(), is_future_valid))
+    {
+       if(is_success())
+           break;
+
+       bool all_ready = std::all_of(futures.begin(), futures.end(), is_future_ready);
+       if(all_ready)
+           break;
+    }*/
+
+    return result;
 }
 
 }
@@ -86,12 +148,35 @@ Result MD5Decoder::Execute()
     time_t f_begin, f_end;
     time(&f_begin);
 
+    auto thread_count = static_cast<size_t>(std::thread::hardware_concurrency());
+    std::cout << "thread count: " << thread_count << "\n";
+
+    std::vector<FutRes> futures;
+    futures.resize(static_cast<size_t>(thread_count));
+
+    constexpr auto all_chars_size = ALL_CHARS.size();
+
     Result result;
 
-    for (auto symbol: ALL_CHARS)
+    auto impl_func = [&](size_t char_idx)
     {
-        std::string tmp_str = { symbol };
-        auto tmp_res = Next(tmp_str);
+        auto symbol = ALL_CHARS[char_idx];
+        std::string tmp_str = {symbol};
+        return Next(tmp_str);
+    };
+
+    for (size_t idx = 0; idx < all_chars_size; idx++)
+    {
+        auto created_thread = std::min(thread_count, all_chars_size - idx);
+        for(size_t i = 0; i < created_thread; i++)
+        {
+            futures[i] = std::async(impl_func, idx);
+            idx++;
+        }
+
+        idx--;
+
+        auto tmp_res = CaclAsyncHash(futures);
         if (tmp_res.mResultType == ResultType::SUCCESS)
         {
             result = tmp_res;
